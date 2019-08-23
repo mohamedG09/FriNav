c081e7ad209b724f79f8be8960c5ab6c017fa311
Mohamed <mohamed.abdelhady9999@gmail.com>
2019-08-23 15:17:33 +0200

    Sign FrontEnd done

--- app/src/main/java/com/example/mapstask/MainActivity.java
+++ app/src/main/java/com/example/mapstask/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.mapstask;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_signup)
+    Button btnSignup;
+    @BindView(R.id.btn_signin)
+    Button btnSignin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_signup)
+    public void onViewClicked() {
+
+        startActivity(new Intent(this,SignUpActivity.class));
+
     }
 }
--- app/src/main/java/com/example/mapstask/SignUpActivity.java
+++ app/src/main/java/com/example/mapstask/SignUpActivity.java
@@ -1,14 +1,53 @@
 package com.example.mapstask;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.kusu.loadingbutton.LoadingButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SignUpActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.btn_signup_act)
+    LoadingButton btnSignupAct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+
+
+
+
+    }
+
+
+    @OnClick(R.id.btn_signup_act)
+    public void btn_signup_act() {
+
+        //btnSignupAct.showLoading();
+
+
+
+
+
+        //TODO Firebase connection with account creation and intents
+
+
+
+
+
+
+        //btnSignupAct.hideLoading();
+
+
     }
 }
